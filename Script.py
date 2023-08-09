ae459e27167d5ca570d677c917e1863bfa3d1362
tejachowdary0 <141829994+tejachowdary0@users.noreply.github.com>
2023-08-10 00:58:48 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -197,7 +197,8 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : <code> {file_name} </code>
+    
 Made by ❤️ @request_bots""" 
 
     IMDB_TEMPLATE_TXT = """
