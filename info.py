a23f92a2327491dc8748de3dcc64885be9f4cf9b
tejachowdary0 <141829994+tejachowdary0@users.noreply.github.com>
2023-08-09 17:51:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,10 +21,10 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "6466129581:AAFYzkMqxddg8Bs37gycBlRIU0fdhE4
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', '')).split()
-NOR_IMG = environ.get("NOR_IMG", "")
+PICS = (environ.get('PICS', 'https://telegra.ph/file/5e45749d1c5f6e6ee9e4e.jpg')).split()
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/5e45749d1c5f6e6ee9e4e.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "")
-SPELL_IMG = environ.get("SPELL_IMG", "")
+SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e45749d1c5f6e6ee9e4e.jpg")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1264280791').split()]
